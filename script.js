77670b06c3b552b40a7559dc475c3094875f78d9
pavanipoloju22 <pavanipoloju22@gmail.com>
2024-12-19 20:10:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function rainon(){
+   let img = document.getElementById('image')
+    img.src="rain-4431_128.gif"
+}
+function rainoff(){
+   let img = document.getElementById('image')
+   img.src="sea-6811812_640.jpg"
+}
